9f0814bf25437d2c9c65b85964360fed476e5e65
Alex Stephens <alexstephens9@gmail.com>
2024-03-19 20:47:19 +0000

    Remove unused mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,21 +6,3 @@ map({ "n", "i", "v" }, "<Up>", "<Nop>")
 map({ "n", "i", "v" }, "<Down>", "<Nop>")
 map({ "n", "i", "v" }, "<Left>", "<Nop>")
 map({ "n", "i", "v" }, "<Right>", "<Nop>")
-
--- M.dap = {
---   plugin = true,
---   n = {
---     ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>"}
---   }
--- }
-
--- M.dap_python = {
---   plugin = true,
---   n = {
---     ["<leader>dpr"] = {
---       function()
---         require('dap-python').test_method()
---       end
---     }
---   }
--- }
