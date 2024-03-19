8754bc391fe3a2d7d3f39faee75ea190c597499e
Alex Stephens <alexstephens9@gmail.com>
2024-03-19 09:56:58 +0000

    Update mappings format - unmap arrow keys

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,26 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map({ "n", "i", "v" }, "<Up>", "<Nop>")
+map({ "n", "i", "v" }, "<Down>", "<Nop>")
+map({ "n", "i", "v" }, "<Left>", "<Nop>")
+map({ "n", "i", "v" }, "<Right>", "<Nop>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- M.dap = {
+--   plugin = true,
+--   n = {
+--     ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>"}
+--   }
+-- }
+
+-- M.dap_python = {
+--   plugin = true,
+--   n = {
+--     ["<leader>dpr"] = {
+--       function()
+--         require('dap-python').test_method()
+--       end
+--     }
+--   }
+-- }
