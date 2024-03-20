3b98578134e9e21f6c9c32760d2beb045b793b02
Alex Stephens <alexstephens9@gmail.com>
2024-03-20 13:51:21 +0000

    Scaffold for overwriting mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,7 +2,13 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- Unmap arrow keys
 map({ "n", "i", "v" }, "<Up>", "<Nop>")
 map({ "n", "i", "v" }, "<Down>", "<Nop>")
 map({ "n", "i", "v" }, "<Left>", "<Nop>")
 map({ "n", "i", "v" }, "<Right>", "<Nop>")
+
+-- Telescope - override some NvChad default mappings
+-- local builtin = require 'telescope.builtin'
+-- vim.api.nvim_del_keymap('n', "<leader>ff")
+-- vim.keymap.set('n', '<leader>fi', builtin.find_files, {desc = "Telescope find files"})
