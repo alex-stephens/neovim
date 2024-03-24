66229168e92054d053f4edc833123b2442768095
Alex Stephens <alexstephens9@gmail.com>
2024-03-24 14:41:25 +0000

    Add tmux navigation to nvim

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,6 +8,11 @@ map({ "n", "i", "v" }, "<Down>", "<Nop>")
 map({ "n", "i", "v" }, "<Left>", "<Nop>")
 map({ "n", "i", "v" }, "<Right>", "<Nop>")
 
+map("n", "<C-h", "<cmd> TmuxNavigateLeft<CR>") 
+map("n", "<C-l", "<cmd> TmuxNavigateRight<CR>") 
+map("n", "<C-j", "<cmd> TmuxNavigateDown<CR>") 
+map("n", "<C-k", "<cmd> TmuxNavigateUp<CR>") 
+
 -- Telescope - override some NvChad default mappings
 -- local builtin = require 'telescope.builtin'
 -- vim.api.nvim_del_keymap('n', "<leader>ff")
